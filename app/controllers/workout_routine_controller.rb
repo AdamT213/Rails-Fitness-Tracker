47e93084af3faaf515ca4ef839ad0c6f4b45ec14
Adam Tropp <adt6261@gmail.com>
2017-12-28 03:01:21 +0000

    built controller action and params to handle submission of workout routine form

--- app/controllers/workout_routine_controller.rb
+++ app/controllers/workout_routine_controller.rb
@@ -5,11 +5,23 @@ class WorkoutRoutineController < ApplicationController
   end
 
   def create
+    @workout_routine = WorkoutRoutine.create(workout_routine_params)
+    if @workout_routine.save
+      redirect_to workout_routine_path(@workout_routine)
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def index
+  end
+
+  private
+
+  def workout_routine_params
+    params.require(:workout_routine).permit(:name, :split_length, :difficulty, :exercise_ids => [])
   end
 end
