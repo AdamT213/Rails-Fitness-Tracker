0477c4267dd62547ed2a816c566573bbeff98518
Adam Tropp <adt6261@gmail.com>
2017-12-28 02:57:26 +0000

    stubbed out controller action and nested form for creating workout routines

---
+++ app/controllers/workout_routine_controller.rb
@@ -0,0 +1,15 @@
+class WorkoutRoutineController < ApplicationController
+
+  def new
+    @workout_routine = WorkoutRoutine.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
