0a71c960d237a28aea6f72e7290035a22536d116
Adam Tropp <adt6261@gmail.com>
2017-12-28 04:22:40 +0000

    stubbed out create action to process new fitness plan form

--- app/controllers/fitness_plan_controller.rb
+++ app/controllers/fitness_plan_controller.rb
@@ -1,2 +1,14 @@
 class FitnessPlanController < ApplicationController
+
+  def new
+  end
+
+  def create
+    workout_routine = params[:workout_routine_id]
+    if current_user
+      current_user.fitness_plans < workout_routine
+    end
+    current_user.save
+    redirect_to user_path(current_user)
+  end
 end
