8234bb09c9c21c7d4b7db806ebe4da65491394e2
Adam Tropp <adt6261@gmail.com>
2017-12-28 02:26:22 +0000

    added controller actions and custom setter/getter to create exercise as part of workout routine

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -5,5 +5,22 @@ class ExercisesController < ApplicationController
   end
 
   def create
+    @exercise = Exercise.create(exercise_params)
+    if params[:workout_routine_id]
+       @exercise.workout_routine = WorkoutRoutine.find[:workout_routine_id]
+      redirect_to workout_routine_path(@exercise.workout_routine)
+    else
+      if @exercise.save
+        redirect_to exercise_path(@exercise)
+      else
+        render :new
+      end
+    end
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :sets, :reps, :target, :workout_routine_name)
   end
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,12 @@
 class Exercise < ActiveRecord::Base
   belongs_to :workout_routine
+
+  def workout_routine_name
+    self.try(:workout_routine).try(:name)
+  end
+
+  def workout_routine_name=(name)
+    workout_routine = WorkoutRoutine.find_or_create_by(name: name)
+    self.workout_routine = workout_routine
+  end
 end
