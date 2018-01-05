695ac6c71b7bd3c7bf317616e109bd011756fc04
Adam Tropp <adt6261@gmail.com>
2018-01-05 00:39:20 +0000

    working on allowing user to edit his/her personal bio and index page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(bio: params[:bio])
+    redirect_to user_path(@user)
+  end
 end
