285039da4b6552fbb8e40a9be7d2440116ec3ab1
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-01-24 22:10:02 +0000

    working on functionality to replace workout routines show page

--- app/assets/javascripts/workoutshow.js
+++ app/assets/javascripts/workoutshow.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(".exercises").on('click', function(e) { 
         $.get("/workout_routines/" + e.target.id, function(data){ 
             debugger;
-            $("#workoutshow").append("<li>" + "Category: " + data["category"] + "</li>")
+            $(".workoutshow").append("<li>" + "Category: " + data["category"] + "</li>")
                 .append("<li>" + "Difficulty: " + data["difficulty"] + "</li>")
         })
     }) 
