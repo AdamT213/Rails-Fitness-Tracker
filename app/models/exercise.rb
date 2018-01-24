4b842d8ae92219ba06ae1789812e2bc02a91cb6b
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-01-24 01:53:01 +0000

    modified changes to nested resource for testing purposes

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,6 +1,6 @@
 class Exercise < ActiveRecord::Base
   belongs_to :workout_routine 
-  validates :name, uniqueness: true
+  validates :name, presence: true
   def workout_routine_name 
     self.try(:workout_routine).try(:name)
   end
