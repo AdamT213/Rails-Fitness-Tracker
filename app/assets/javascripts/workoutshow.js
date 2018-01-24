304f800d84f408a93d44aef4dc637466d895062f
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-01-24 21:44:11 +0000

    updated formatting on workout routines index page for clarity

--- app/assets/javascripts/workoutshow.js
+++ app/assets/javascripts/workoutshow.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $(".exercises").on('click', function(e) { 
-        debugger; 
-        console.log(e)
+        $.get("/workout_routines/" + e.target.id, function(data){ 
+            debugger;
+            $("#workoutshow").append("<li>" + "Category: " + data["category"] + "</li>")
+                .append("<li>" + "Difficulty: " + data["difficulty"] + "</li>")
+        })
     }) 
 })
