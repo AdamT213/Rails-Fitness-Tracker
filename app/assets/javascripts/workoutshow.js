36771b660d7ea000f4172261179c1ee12fe75bee
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-01-25 01:07:15 +0000

    added logic so that only first time pressing view exercises appends to the DOM

--- app/assets/javascripts/workoutshow.js
+++ app/assets/javascripts/workoutshow.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(".exercises").on('click', function(e) { 
         $.get("/workout_routines/" + e.target.id, function(data){ 
+            if ($("div#"+data.id + ".workoutshow")[0]["childElementCount"] === 0) {
             $("div#"+data.id + ".workoutshow").append("<li>" + "Category: " + data["category"] + "</li>")  
                 .append("Exercises:" + "<br>" + "<ul>")
                 for (let i=0; i < data["exercises"].length; i++) { 
@@ -8,7 +9,8 @@ $(document).ready(function(){
                     .append("<li>" + "Target: " + data["exercises"][i]["target"] + "</li>")
                     .append("<li>" + "Sets: " + data["exercises"][i]["sets"] + "</li>")
                     .append("<li>" + "Reps: " + data["exercises"][i]["reps"] + "</li>" + "</ul>") 
-                }
+                } 
+            }
         })
     }) 
 })
