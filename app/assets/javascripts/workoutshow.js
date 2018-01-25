0e41ca2c2c495b3e592fb46c972bf912c37cf2d0
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-01-25 00:37:39 +0000

    got selector and content for workout show working properly

--- app/assets/javascripts/workoutshow.js
+++ app/assets/javascripts/workoutshow.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
     $(".exercises").on('click', function(e) { 
         $.get("/workout_routines/" + e.target.id, function(data){ 
             debugger;
-            $(".workoutshow").append("<li>" + "Category: " + data["category"] + "</li>")
-                .append("<li>" + "Difficulty: " + data["difficulty"] + "</li>")
+            $("div#"+data.id + ".workoutshow").append("<li>" + "Category: " + data["category"] + "</li>")  
+                .append("Exercises:" + "<br>" + "<ul>")
+                for (let i=0; i < data["exercises"].length; i++) { 
+                  $("div#"+data.id + ".workoutshow").append("<li>" + "Name: " + data["exercises"][i]["name"] + "</li>") 
+                    .append("<li>" + "Target: " + data["exercises"][i]["target"] + "</li>")
+                    .append("<li>" + "Sets: " + data["exercises"][i]["sets"] + "</li>")
+                    .append("<li>" + "Reps: " + data["exercises"][i]["reps"] + "</li>" + "</ul>") 
+                }
         })
     }) 
 })
