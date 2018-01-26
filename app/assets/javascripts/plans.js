7bda078a27085a19bbf7fe02d2de7c9c4a408dd5
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-01-26 00:55:27 +0000

    got fitness plans index to load with regex

--- app/assets/javascripts/plans.js
+++ app/assets/javascripts/plans.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){ 
     $("#userplans").on('click', function(e) { 
-        debugger;
+        var userId = e.target.baseURI.match(/\d+$/)[0] 
         $.get("/fitness_plans", function(data) { 
             for (let i=0; i<data.length; i++) { 
-                if (data[i]["user_id"] === userId.innerText) { 
-                    $("#plans").append("<li>" + "Name: " + data[i]["workout_routine"]["name"] + "</li>") 
-                        .append("<li>" + "Length(days): " + data[i]["duration"] + "</li>")
-                        .append("<li>" + "Category: " + data[i]["workout_routine"]["category"] + "</li>") 
-                        .append("<li>" + "Difficulty: " + data[i]["difficulty"] + "</li>") 
-                        .append("<li>" + "Days per Week: " + data[i]["split_length"] + "</li>") 
+                if (data[i]["user_id"].toString() === userId) {  
+                    if (data[i]["workout_routine"] !== null) {
+                        $("#plans").append("<li>" + "Name: " + data[i]["workout_routine"]["name"] + "</li>") 
+                            .append("<li>" + "Length(days): " + data[i]["duration"] + "</li>")
+                            .append("<li>" + "Category: " + data[i]["workout_routine"]["category"] + "</li>") 
+                            .append("<li>" + "Difficulty: " + data[i]["workout_routine"]["difficulty"] + "</li>") 
+                            .append("<li>" + "Days per Week: " + data[i]["workout_routine"]["split_length"] + "</li>") 
+                    }
                 }
             }
         })
